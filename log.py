b0b7ab3bce0fb7c39927dde4bf9ab0ae42cb45c1
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2025-09-29 21:49:46 +0200

    integracion parcial del log, menus terminados, funcion de emparejar

--- log.py
+++ log.py
@@ -1,7 +1,17 @@
-'''En esta clase hacer toda la gestion del log'''
-from datetime import datetime
-import logging as log
+import logging
+import os
 
-
-fecha = datetime.now().strftime("%d%m%Y").lower()
-nombre_fichero = f"log/{fecha}_HEROESYVILLANOS.log"
+def configurar_logging():
+    # Crear directorio log si no existe
+    if not os.path.exists('log'):
+        os.makedirs('log')
+    
+    fecha = datetime.now().strftime("%d%m%Y")
+    nombre_fichero = f"log/{fecha}_HEROESYVILLANOS.log"
+    
+    logging.basicConfig(
+        filename=nombre_fichero,
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%d/%m/%Y %H:%M:%S'
+    )
--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ from clases.CONSTANTES import Constantes
 from clases.heroe import Heroe
 import datetime
 import numpy as np
+import logging
 
 from clases.villano import Villano
 
@@ -11,7 +12,8 @@ def menu():
     print("1) Para crear Heroe")
     print("2) Para crear Villano")
     print("3) Para buscar un heroe o villano")
-    print("4) Para salir")
+    print("4) Para simular una pelea entre un villano y un héroe")
+    print("5) Para salir")
 
 def menuFiltro():
     print('1) Para buscar Heroe')
@@ -54,7 +56,7 @@ def main():
         while True:
             menu()
             opcion = int(input("Que eliges"))
-            if opcion >= 5:
+            if opcion >= 6:
                 print("Selecciona una opcion valida")
             else:
                 gestionAulaDeHeroesYVillanos(opcion)
@@ -69,7 +71,18 @@ if __name__ == "__main__":
 
 
 def gestionAulaDeHeroesYVillanos(opcion):
-    pass
+    match opcion:
+        case 1:
+            crearHeroe()
+        case 2:
+            crearVillano()
+        case 3:
+            funcionFiltro()
+        case 4:
+            emparejarHeroeVillano()
+        case 5:
+            exit()
+
 
 
 idPersonajes = 0
@@ -77,91 +90,109 @@ listaPersonajes = []
 
 
 def crearHeroe():
+    global idPersonajes  # Necesitamos modificar la variable global, sacado del gpt que no sabia como iba en py
     nombre = input("Nombre: ")
     apellidos = input("Apellidos: ")
     fecha = sacarFecha()
 
-    codigoLimpio = 0
-    bienDocumentado = 0
-    gitGod = 0
-    arquitecto = 0
-    detallista = 0
+
+    codigoLimpio = random.randint(Constantes.VALOR_MINIMO, Constantes.VALOR_MAXIMO)
+    bienDocumentado = random.randint(Constantes.VALOR_MINIMO, Constantes.VALOR_MAXIMO)
+    gitGod = random.randint(Constantes.VALOR_MINIMO, Constantes.VALOR_MAXIMO)
+    arquitecto = random.randint(Constantes.VALOR_MINIMO, Constantes.VALOR_MAXIMO)
+    detallista = random.randint(Constantes.VALOR_MINIMO, Constantes.VALOR_MAXIMO)
+
 
     atributos = [codigoLimpio, bienDocumentado, gitGod, arquitecto, detallista]
-    for puntuacion in atributos:
-        atributos[puntuacion] = random.randint(Constantes.VALOR_MINIMO, Constantes.VALOR_MAXIMO)
     puntuacion = np.mean(atributos)
 
+
+    idPersonajes += 1
     heroe = Heroe(nombre,
                   apellidos,
                   fecha,
-                  ++idPersonajes,
+                  idPersonajes,
                   puntuacion,
                   codigoLimpio,
                   bienDocumentado,
                   gitGod, arquitecto
                   , detallista)
     listaPersonajes.append(heroe)
+    logging.info(f'Héroe creado: {nombre} {apellidos} con ID {idPersonajes} y puntuación {puntuacion}')
 
 
 def crearVillano():
+    global idPersonajes
     nombre = input("Nombre: ")
     apellidos = input("Apellidos: ")
     fecha = sacarFecha()
 
-    chagepeteador = 0
-    entregadorTardio = 0
-    ausencias = 0
-    hablador = 0
+    chagepeteador = random.randint(Constantes.VALOR_MINIMO, Constantes.VALOR_MAXIMO)
+    entregadorTardio = random.randint(Constantes.VALOR_MINIMO, Constantes.VALOR_MAXIMO)
+    ausencias = random.randint(Constantes.VALOR_MINIMO, Constantes.VALOR_MAXIMO)
+    hablador = random.randint(Constantes.VALOR_MINIMO, Constantes.VALOR_MAXIMO)
 
     atributos = [chagepeteador, entregadorTardio, ausencias, hablador]
-    for puntuacion in atributos:
-        atributos[puntuacion] = random.randint(Constantes.VALOR_MINIMO, Constantes.VALOR_MAXIMO)
     puntuacion = np.mean(atributos)
 
+    idPersonajes += 1
     villano = Villano(nombre,
                       apellidos,
                       fecha,
-                      ++idPersonajes,
+                      idPersonajes,
                       puntuacion,
                       chagepeteador,
                       entregadorTardio,
                       ausencias,
                       hablador)
     listaPersonajes.append(villano)
+    logging.info(f'Villano creado: {nombre} {apellidos} con ID {idPersonajes} y puntuación {puntuacion}')
+
+
+def emparejarHeroeVillano():
+
+    heroes = [p for p in listaPersonajes if type(p) == Heroe]
+    villanos = [p for p in listaPersonajes if type(p) == Villano]
+
+
+    heroe = random.choice(heroes)
+    villano = random.choice(villanos)
+
+    logging.info(f"Emparejamiento: {heroe.returnNombre()} vs {villano.returnNombre()}")
+
 
 
 def funcionFiltro():
     while True:
         menuFiltro()
         opcion = int(input())
-        if opcion==2:
+        if opcion == 2:
             villanos = [p for p in listaPersonajes if type(p) == Villano]
             menuFiltroVillano()
-            opcion2 = int(input('Elige'))
+            opcion2 = int(input('Elige: '))
             match opcion2:
                 case 1:
-                    nombre = input('Escribe el nombre').lower()
+                    nombre = input('Escribe el nombre: ').lower()
                     for villano in villanos:
-                        if villano.returnNombre().lower == nombre:
+                        if villano.returnNombre().lower() == nombre:
                             print('Villano encontrado!')
                             print(villano)
                 case 2:
-                    apellido = input('Escribe el apellido').lower()
+                    apellido = input('Escribe el apellido: ').lower()
                     for villano in villanos:
-                        if villano.returnApellidos().lower == apellido:
+                        if villano.returnApellidos().lower() == apellido:
                             print('Villano encontrado!')
                             print(villano)
                 case 3:
-                    idv = int(input('Escribe la ID'))
+                    idv = int(input('Escribe la ID: '))
                     for villano in villanos:
                         if villano.returnId() == idv:
                             print('Villano encontrado!')
                             print(villano)
                 case 4:
                     menuMayorMenor()
-                    opcion3 = int(input('Elige'))
-                    cantidad = int(input('Y ahora el numero'))
+                    opcion3 = int(input('Elige: '))
+                    cantidad = int(input('Y ahora el numero: '))
                     match opcion3:
                         case 1:
                             for villano in villanos:
@@ -178,10 +209,12 @@ def funcionFiltro():
                                 if villano.returnChagepeteador() == cantidad:
                                     print('Villano encontrado!')
                                     print(villano)
+                        case _:
+                            print('Opcion incorrecta')
                 case 5:
                     menuMayorMenor()
-                    opcion3 = int(input('Elige'))
-                    cantidad = int(input('Y ahora el numero'))
+                    opcion3 = int(input('Elige: '))
+                    cantidad = int(input('Y ahora el numero: '))
                     match opcion3:
                         case 1:
                             for villano in villanos:
@@ -198,10 +231,12 @@ def funcionFiltro():
                                 if villano.returnEntregadorTardio() == cantidad:
                                     print('Villano encontrado!')
                                     print(villano)
+                        case _:
+                            print('Opcion incorrecta')
                 case 6:
                     menuMayorMenor()
-                    opcion3 = int(input('Elige'))
-                    cantidad = int(input('Y ahora el numero'))
+                    opcion3 = int(input('Elige: '))
+                    cantidad = int(input('Y ahora el numero: '))
                     match opcion3:
                         case 1:
                             for villano in villanos:
@@ -218,10 +253,12 @@ def funcionFiltro():
                                 if villano.returnHablador() == cantidad:
                                     print('Villano encontrado!')
                                     print(villano)
+                        case _:
+                            print('Opcion incorrecta')
                 case 7:
                     menuMayorMenor()
-                    opcion3 = int(input('Elige'))
-                    cantidad = int(input('Y ahora el numero'))
+                    opcion3 = int(input('Elige: '))
+                    cantidad = int(input('Y ahora el numero: '))
                     match opcion3:
                         case 1:
                             for villano in villanos:
@@ -238,39 +275,156 @@ def funcionFiltro():
                                 if villano.returnAusencias() == cantidad:
                                     print('Villano encontrado!')
                                     print(villano)
+                        case _:
+                            print('Opcion incorrecta')
                 case 8:
                     for villano in villanos:
                         print(villano)
-
                 case 9:
-                    print('Saliendo')
-                    exit()
+                    print('Saliendo del filtro de villanos')
+                    break
+                case _:
+                    print('Opcion incorrecta')
 
-        elif opcion==1:
+        elif opcion == 1:
+            heroes = [p for p in listaPersonajes if type(p) == Heroe]
             menuFiltroHeroe()
-            opcion2 = int(input('Elige'))
+            opcion2 = int(input('Elige: '))
             match opcion2:
                 case 1:
-                    pass
+                    nombre = input('Escribe el nombre: ').lower()
+                    for heroe in heroes:
+                        if heroe.returnNombre().lower() == nombre:
+                            print('Héroe encontrado!')
+                            print(heroe)
                 case 2:
-                    pass
+                    apellido = input('Escribe el apellido: ').lower()
+                    for heroe in heroes:
+                        if heroe.returnApellidos().lower() == apellido:
+                            print('Héroe encontrado!')
+                            print(heroe)
                 case 3:
-                    pass
+                    idv = int(input('Escribe la ID: '))
+                    for heroe in heroes:
+                        if heroe.returnId() == idv:
+                            print('Héroe encontrado!')
+                            print(heroe)
                 case 4:
-                    pass
+                    menuMayorMenor()
+                    opcion3 = int(input('Elige: '))
+                    cantidad = int(input('Y ahora el numero: '))
+                    match opcion3:
+                        case 1:
+                            for heroe in heroes:
+                                if heroe.returnCodigoLimpio() > cantidad:
+                                    print('Héroe encontrado!')
+                                    print(heroe)
+                        case 2:
+                            for heroe in heroes:
+                                if heroe.returnCodigoLimpio() < cantidad:
+                                    print('Héroe encontrado!')
+                                    print(heroe)
+                        case 3:
+                            for heroe in heroes:
+                                if heroe.returnCodigoLimpio() == cantidad:
+                                    print('Héroe encontrado!')
+                                    print(heroe)
+                        case _:
+                            print('Opcion incorrecta')
                 case 5:
-                    pass
+                    menuMayorMenor()
+                    opcion3 = int(input('Elige: '))
+                    cantidad = int(input('Y ahora el numero: '))
+                    match opcion3:
+                        case 1:
+                            for heroe in heroes:
+                                if heroe.returnBienDocumentado() > cantidad:
+                                    print('Héroe encontrado!')
+                                    print(heroe)
+                        case 2:
+                            for heroe in heroes:
+                                if heroe.returnBienDocumentado() < cantidad:
+                                    print('Héroe encontrado!')
+                                    print(heroe)
+                        case 3:
+                            for heroe in heroes:
+                                if heroe.returnBienDocumentado() == cantidad:
+                                    print('Héroe encontrado!')
+                                    print(heroe)
+                        case _:
+                            print('Opcion incorrecta')
                 case 6:
-                    pass
+                    menuMayorMenor()
+                    opcion3 = int(input('Elige: '))
+                    cantidad = int(input('Y ahora el numero: '))
+                    match opcion3:
+                        case 1:
+                            for heroe in heroes:
+                                if heroe.returnGitgod() > cantidad:
+                                    print('Héroe encontrado!')
+                                    print(heroe)
+                        case 2:
+                            for heroe in heroes:
+                                if heroe.returnGitgod() < cantidad:
+                                    print('Héroe encontrado!')
+                                    print(heroe)
+                        case 3:
+                            for heroe in heroes:
+                                if heroe.returnGitgod() == cantidad:
+                                    print('Héroe encontrado!')
+                                    print(heroe)
+                        case _:
+                            print('Opcion incorrecta')
                 case 7:
-                    pass
+                    menuMayorMenor()
+                    opcion3 = int(input('Elige: '))
+                    cantidad = int(input('Y ahora el numero: '))
+                    match opcion3:
+                        case 1:
+                            for heroe in heroes:
+                                if heroe.returnArquitecto() > cantidad:
+                                    print('Héroe encontrado!')
+                                    print(heroe)
+                        case 2:
+                            for heroe in heroes:
+                                if heroe.returnArquitecto() < cantidad:
+                                    print('Héroe encontrado!')
+                                    print(heroe)
+                        case 3:
+                            for heroe in heroes:
+                                if heroe.returnArquitecto() == cantidad:
+                                    print('Héroe encontrado!')
+                                    print(heroe)
+                        case _:
+                            print('Opcion incorrecta')
                 case 8:
-                    pass
+                    menuMayorMenor()
+                    opcion3 = int(input('Elige: '))
+                    cantidad = int(input('Y ahora el numero: '))
+                    match opcion3:
+                        case 1:
+                            for heroe in heroes:
+                                if heroe.returnDetallista() > cantidad:
+                                    print('Héroe encontrado!')
+                                    print(heroe)
+                        case 2:
+                            for heroe in heroes:
+                                if heroe.returnDetallista() < cantidad:
+                                    print('Héroe encontrado!')
+                                    print(heroe)
+                        case 3:
+                            for heroe in heroes:
+                                if heroe.returnDetallista() == cantidad:
+                                    print('Héroe encontrado!')
+                                    print(heroe)
+                        case _:
+                            print('Opcion incorrecta')
                 case 9:
-                    pass
+                    for heroe in heroes:
+                        print(heroe)
                 case 10:
-                    print('Saliendo')
-                    exit()
+                    print('Saliendo del filtro de héroes')
+                    break
                 case _:
                     print('Opcion incorrecta')
         else:
